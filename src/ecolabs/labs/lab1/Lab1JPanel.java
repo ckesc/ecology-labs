9a82e84c51dca4a73cd35974542a86abe88231c3
FormularHunter <FormularHunter@9e1b3c90-df0f-e2da-528b-f5ba91d05001>
2011-11-19 15:17:20 +0000

    lab 1 + jFreeChart

--- src/ecolabs/labs/lab1/Lab1JPanel.java
+++ src/ecolabs/labs/lab1/Lab1JPanel.java
@@ -11,6 +11,7 @@
 package ecolabs.labs.lab1;
 
 import ecolabs.labs.LabJPanel;
+import org.jfree.chart.JFreeChart;
 
 /**
  *
@@ -18,10 +19,43 @@ import ecolabs.labs.LabJPanel;
  */
 public class Lab1JPanel extends LabJPanel {
 
+    /**
+     * Средний медианный размер капель воды, мм
+     */
+    double Dh2o;
+    /**
+     * Cредний медианный размер частиц пыли, мкм
+     */
+    double Dsol;
+    /**
+     * Расход воды для орошения газа, м3/с;
+     */
+    double Vaq;
+    /**
+     * объемный расход газа на выходе из скруббера при рабочих условиях,
+     * м3/с;
+     */
+    double Va;
+    /**
+     *  Коэффициент захвата частицы каплей воды;
+     */
+    double ησ;
+    
     /** Creates new form Lab1JPanel */
     public Lab1JPanel() {
         initComponents();
         Caption = "Лабораторная работа №1";
+    }
+
+    /**
+     * Степень очистки газа
+     * @return 
+     */
+    public double η(double H)
+    {
+        double numerator  = -3 * Vaq * ησ * H;
+        double denominator = 2 * Va * Dh2o * 0.001f;
+        return 1 - (double) Math.exp(numerator / denominator);
     }
 
     /** This method is called from within the constructor to
@@ -35,8 +69,10 @@ public class Lab1JPanel extends LabJPanel {
 
         jComboBoxVar = new javax.swing.JComboBox();
         jLabelVar = new javax.swing.JLabel();
-
-        setName("Form"); // NOI18N
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
 
         jComboBoxVar.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1" }));
         jComboBoxVar.setName("jComboBoxVar"); // NOI18N
@@ -45,31 +81,70 @@ public class Lab1JPanel extends LabJPanel {
         jLabelVar.setText(resourceMap.getString("jLabelVar.text")); // NOI18N
         jLabelVar.setName("jLabelVar"); // NOI18N
 
+        jLabel1.setText(resourceMap.getString("jLabelFormula.text")); // NOI18N
+        jLabel1.setName("jLabelFormula"); // NOI18N
+
+        jPanel1.setName("jPanelScrubber"); // NOI18N
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setIcon(resourceMap.getIcon("jLabel2.icon")); // NOI18N
+        jLabel2.setText(resourceMap.getString("jLabel2.text")); // NOI18N
+        jLabel2.setName("jLabel2"); // NOI18N
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 270));
+
+        jPanel2.setName("jPanelCharts"); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 570, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 531, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabelVar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabelVar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jComboBoxVar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jComboBoxVar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(307, Short.MAX_VALUE))
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelVar)
-                    .addComponent(jComboBoxVar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(269, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabelVar)
+                            .addComponent(jComboBoxVar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 419, Short.MAX_VALUE)))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox jComboBoxVar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabelVar;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
